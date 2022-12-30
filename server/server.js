8c5ee76cf2bc7ff77df49867521d0e8ffab087c1
Sakshi Ramdurgekar <sakshi.rdkar@gmail.com>
2022-12-29 23:06:54 -0500

    added url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://main--starlit-kelpie-218fc8.netlify.app/",
+    ],
     credentials: true,
   })
 );
